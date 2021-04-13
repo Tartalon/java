228314342d6ee300035ec9025a28bc50ee2090d7
Vladimir <50200804+Tartalon@users.noreply.github.com>
2021-04-13 17:19:15 +0300

    jb002 task_01

--- jb002_les/src/task_01/MyArea.java
+++ jb002_les/src/task_01/MyArea.java
@@ -1,4 +1,9 @@
 package task_01;
 
 public class MyArea {
+    private static final float PI = 3.14F;
+
+    static void areaOfCircle (double radius) {
+        System.out.println("Площадь круга: " + PI * (Math.pow(radius, 2)));
+    }
 }
