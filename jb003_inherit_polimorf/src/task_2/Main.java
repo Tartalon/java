cdafcb7db2740396f4610719586852fe0fceaaa0
Vladimir <50200804+Tartalon@users.noreply.github.com>
2021-04-16 19:03:37 +0300

    jb003 task_02 done

--- jb003_inherit_polimorf/src/task_2/Main.java
+++ jb003_inherit_polimorf/src/task_2/Main.java
@@ -1,4 +1,13 @@
 package task_2;
 
 public class Main {
+    public static void main(String[] args) {
+
+        ClassRoom[] room = new ClassRoom[3];
+
+        room[0] = new ClassRoom(new ExcellentPupil());
+        room[1] = new ClassRoom(new GoodPupil());
+        room[2] = new ClassRoom(new BedPupil());
+
+    }
 }
--- jb003_inherit_polimorf/src/task_2/Pupil.java
+++ jb003_inherit_polimorf/src/task_2/Pupil.java
@@ -37,3 +37,41 @@ class ExcellentPupil extends Pupil {
         System.out.println("Excellent relax");
     }
 }
+
+class GoodPupil extends Pupil {
+    @Override
+    void study() {
+        System.out.println("Good study");
+    }
+    @Override
+    void read() {
+        System.out.println("Good read");
+    }
+    @Override
+    void write() {
+        System.out.println("Good write");
+    }
+    @Override
+    void relax() {
+        System.out.println("Good relax");
+    }
+}
+
+class BedPupil extends Pupil {
+    @Override
+    void study() {
+        System.out.println("Bed study");
+    }
+    @Override
+    void read() {
+        System.out.println("Bed read");
+    }
+    @Override
+    void write() {
+        System.out.println("Bed write");
+    }
+    @Override
+    void relax() {
+        System.out.println("Bed relax");
+    }
+}
